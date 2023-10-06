8ed037227f43bf1840926a2d6f88e756de331d67
Kingloo <k1ngloo@hotmail.com>
2023-10-06 22:51:29 +0200

    Command line options: -p for port and -d for directory

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -13,10 +14,13 @@ namespace quicklocalfileserver
 {
 	public static class Program
 	{
-		private const int port = 12_000;
+		private const int defaultPort = 12_000;
+		private static readonly string defaultDirectory = AppContext.BaseDirectory;
 		
 		public static async Task<int> Main(string[] args)
 		{
+			ArgumentNullException.ThrowIfNull(args);
+
 			CancellationTokenSource cts = new CancellationTokenSource();
 
 			Console.CancelKeyPress += (object? _, ConsoleCancelEventArgs e) =>
@@ -25,12 +29,19 @@ namespace quicklocalfileserver
 
 				cts.Cancel();
 			};
+
+			int port = GetPort(args);
+			string directory = GetDirectory(args);
 			
-			WebApplication webApplication = BuildWebApplication();
+			WebApplication webApplication = BuildWebApplication(port, directory);
 
 			try
 			{
-				await webApplication.RunAsync(cts.Token).ConfigureAwait(false);
+				await webApplication.StartAsync(cts.Token).ConfigureAwait(false);
+
+				await Console.Out.WriteLineAsync($"serving from '{directory}' on port {port}").ConfigureAwait(false);
+
+				await webApplication.WaitForShutdownAsync(cts.Token).ConfigureAwait(false);
 			}
 			finally
 			{
@@ -42,20 +53,81 @@ namespace quicklocalfileserver
 			return 0;
 		}
 
-		private static WebApplication BuildWebApplication()
+		private static int GetPort(string[] args)
+		{
+			return GetValueFromCommandLineArgs(args, "-p") switch
+			{
+				string value => Int32.TryParse(value, out int result) switch
+				{
+					true => IsPortInRange(result)
+						? result
+						: throw new ArgumentOutOfRangeException($"invalid port number ('{result}')"),
+					false => throw new ArgumentOutOfRangeException($"not an integer ('{value}')")
+				},
+				_ => defaultPort
+			};
+		}
+
+		private static bool IsPortInRange(int port)
+		{
+			return port > 1023 && port < 65536;
+		}
+
+		private static string GetDirectory(string[] args)
+		{
+			string? absolutePath = defaultDirectory;
+
+			if (GetValueFromCommandLineArgs(args, "-d") is string value)
+			{
+				if (value.Contains('~', StringComparison.Ordinal))
+				{
+					value = value.Replace(
+						"~",
+						Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+						StringComparison.Ordinal);
+				}
+
+				absolutePath = Path.IsPathRooted(value) ? value : Path.GetFullPath(value);
+			}
+
+			if (!Directory.Exists(absolutePath))
+			{
+				throw new DirectoryNotFoundException($"{absolutePath}");
+			}
+
+			return absolutePath;
+		}
+
+		private static string? GetValueFromCommandLineArgs(string[] args, string switchName)
+		{
+			for (int i = 0; i < args.Length; i++)
+			{
+				if (String.Equals(switchName, args[i], StringComparison.Ordinal))
+				{
+					if (i + 1 < args.Length)
+					{
+						return args[i + 1];
+					}
+				}
+			}
+
+			return null;
+		}
+
+		private static WebApplication BuildWebApplication(int port, string directory)
 		{
 			WebApplicationBuilder webAppBuilder = WebApplication.CreateBuilder(new WebApplicationOptions
 			{
 				ApplicationName = "quicklocalfileserver"
 			});
 
-			webAppBuilder.WebHost.ConfigureLogging((ILoggingBuilder loggingBuilder) =>
+			webAppBuilder.WebHost.ConfigureLogging(static (ILoggingBuilder loggingBuilder) =>
 			{
 				loggingBuilder.ClearProviders();
 
 				loggingBuilder.SetMinimumLevel(LogLevel.Warning);
 
-				loggingBuilder.AddSimpleConsole((SimpleConsoleFormatterOptions simpleConsoleFormatterOptions) =>
+				loggingBuilder.AddSimpleConsole(static (SimpleConsoleFormatterOptions simpleConsoleFormatterOptions) =>
 				{
 					simpleConsoleFormatterOptions.ColorBehavior = LoggerColorBehavior.Enabled;
 					simpleConsoleFormatterOptions.IncludeScopes = true;
@@ -72,7 +144,7 @@ namespace quicklocalfileserver
 
 			webApplication.UseStaticFiles(new StaticFileOptions
 			{
-				FileProvider = new PhysicalFileProvider(AppContext.BaseDirectory)
+				FileProvider = new PhysicalFileProvider(directory)
 			});
 
 			return webApplication;
